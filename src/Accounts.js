94aa09b7fe291c24b5d22998376802c5051e155e
Edwin <huangzuizui@gmail.com>
2023-11-09 12:57:37 +0800

    fix: return error instead of throw it in Accounts

--- src/Accounts.js
+++ src/Accounts.js
@@ -1,4 +1,5 @@
 const Data = require('./data').default;
+
 class Accounts {
   constructor() {
     this.data = new Data();
@@ -6,22 +7,25 @@ class Accounts {
 
   register({ role, name, password }) {
     if (!role || !name || !password) {
-      throw new Error('Invalid account information');
+      return new Error('Invalid account information');
     }
     if (this.data.find({ name }).length > 0) {
-      throw new Error('Account already exists');
+      return new Error('Account already exists');
     }
     if (role !== 'admin' && role !== 'user') {
-      throw new Error('Invalid account role');
+      return new Error('Invalid account role');
     }
     return this.data.add({ role, name, password });
   }
 
   login({ name, password }) {
     if (!name || !password) {
-      throw new Error('Invalid account information');
+      return new Error('Invalid account information');
     }
     const [account] = this.data.find({ name, password });
+    if (!account) {
+      return new Error('Account not exists');
+    }
     return account;
   }
 }
--- tests/Accounts.test.js
+++ tests/Accounts.test.js
@@ -2,24 +2,34 @@ const Accounts = require('../src/Accounts').default;
 describe('Test register account', () => {
   const accounts = new Accounts();
 
-  it('should return registered account', () => {
+  it('should success and return registered account', () => {
     const account = accounts.register({ role: 'admin', name: 'Tom', password: '123' });
     expect(account).toEqual({ id: 0, role: 'admin', name: 'Tom', password: '123' });
   });
 
   it('should verify arguments', () => {
-    expect(() => accounts.register({ role: 'user', password: '321' })).toThrow('Invalid account information');
-    expect(() => accounts.register({ password: '321' })).toThrow('Invalid account information');
-    expect(() => accounts.register({ role: 'user' })).toThrow('Invalid account information');
+    const error = accounts.register({ role: 'user', password: '321' });
+    expect(error).toBeInstanceOf(Error);
+    expect(error.message).toBe('Invalid account information');
+    const error1 = accounts.register({ password: '321' });
+    expect(error1).toBeInstanceOf(Error);
+    expect(error1.message).toBe('Invalid account information');
+    const error2 = accounts.register({ role: 'user' });
+    expect(error2).toBeInstanceOf(Error);
+    expect(error2.message).toBe('Invalid account information');
   });
 
   it('should check if account is registered by name', () => {
     accounts.register({ role: 'admin', name: 'Jim', password: '123' });
-    expect(() => accounts.register({ role: 'user', name: 'Jim', password: '321' })).toThrow('Account already exists');
+    const error = accounts.register({ role: 'user', name: 'Jim', password: '321' });
+    expect(error).toBeInstanceOf(Error);
+    expect(error.message).toBe('Account already exists');
   })
 
   it('should check if account is registered by role user or admin', () => {
-    expect(() => accounts.register({ role: 'super', name: 'Jack', password: '321' })).toThrow('Invalid account role');
+    const error = accounts.register({ role: 'super', name: 'Jack', password: '321' });
+    expect(error).toBeInstanceOf(Error);
+    expect(error.message).toBe('Invalid account role');
   });
 });
 
@@ -28,18 +38,23 @@ describe('Test login account', () => {
   accounts.register({ role: 'admin', name: 'Tom', password: '123' });
   accounts.register({ role: 'user', name: 'Jim', password: '123' });
 
-  it('should return account', () => {
+  it('should success and return account', () => {
     const account = accounts.login({ name: 'Tom', password: '123' });
     expect(account).toEqual({ id: 0, role: 'admin', name: 'Tom', password: '123' });
   });
 
   it('should verify arguments', () => {
-    expect(() => accounts.login({ role: 'Tom' })).toThrow('Invalid account information');
-    expect(() => accounts.login({ password: '321' })).toThrow('Invalid account information');
+    const error = accounts.login({ name: 'Tom' });
+    expect(error).toBeInstanceOf(Error);
+    expect(error.message).toBe('Invalid account information');
+    const error1 = accounts.login({ password: '321' });
+    expect(error1).toBeInstanceOf(Error);
+    expect(error1.message).toBe('Invalid account information');
   });
 
-  it('should return undefined if account is not registered', () => {
+  it('should fail if account not exit', () => {
     const account = accounts.login({ name: 'Jack', password: '123' });
-    expect(account).toEqual(undefined);
+    expect(account).toBeInstanceOf(Error);
+    expect(account.message).toBe('Account not exists');
   });
 });
